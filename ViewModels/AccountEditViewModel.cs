b748bc62aad3ac574b0ecd94db823fbd5cac73ff
Madji ali Garba <madjiali2@gmail.com>
2022-09-12 09:43:36 +0200

    End searchfunctionality

---
+++ ViewModels/AccountEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using certificationRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace certificationRegister.ViewModels
+{
+    public class AccountEditViewModel:AppUser
+    {
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirmation do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
